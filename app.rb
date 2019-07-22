9ecbff5d3f010aa448084a9e41ae9ca69f33d361
Gaspard49 <gaspard.tertrais@gmail.com>
2019-07-22 19:06:57 +0200

    finish

--- app.rb
+++ app.rb
@@ -1,11 +1,19 @@
 # lignes très pratiques qui appellent les gems du Gemfile. On verra plus tard comment s'en servir ;) - ça évite juste les "require" partout
+require 'pry'
 require 'bundler'
-Bundler.require
+
+
 
 # lignes qui appellent les fichiers lib/user.rb et lib/event.rb
 # comme ça, tu peux faire User.new dans ce fichier d'application. Top.
 require_relative 'lib/user'
 require_relative 'lib/event'
+
+puts gas = User.new('gaspard.tertrais@gmail.com', 25)
+puts mar =Event.new('2019-09-21', 30)
+
+
+binding.pry
 
 
 # Maintenant c'est open bar pour tester ton application. Tous les fichiers importants sont chargés
--- lib/user.rb
+++ lib/user.rb
@@ -1,3 +1,48 @@
+# frozen_string_literal: true
+
+require 'pry'
+
 class User
+  attr_accessor :email, :age
+  @@all_user = []
+
+  def initialize(email_to_save, age_to_save)
+    if check_email(email_to_save) && check_age(age_to_save)
+      @email = email_to_save
+      @age = age_to_save
+      @@all_user <<  [@email, @age]
+    else
+      p 'Please enter a valid email AND a valid age!'
+    end
+  end
+
+  
+
+  def self.all
+    return @@all_user 
+  end
+
+  def self.find_by_email(email)
+    i=0
+    self.all.each do |val|
+      if self.all[i][0] == email
+        puts "The User age is: #{self.all[i][1]} years old."
+      end
+      i += 1
+    end
+  end
 
 end
+
+private
+
+def check_email(email_to_save)
+  true if email_to_save.include?('@') && email_to_save.include?('.')
+end
+
+def check_age(age_to_save)
+  true if age_to_save.is_a? Integer
+end
+
+binding.pry
+p 'end of file'
